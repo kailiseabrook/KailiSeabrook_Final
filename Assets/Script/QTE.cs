3764d40c5dc6e7758968423b7f994a16370d6e02
kailiseabrook <kailiseabrook@lcad.edu>
2024-11-26 13:44:29 -0800

    1
    add words

--- Assets/Script/QTE.cs
+++ Assets/Script/QTE.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class QTE : MonoBehaviour
 {
     // Start is called before the first frame update
-    public string easyWords = "Bone";
+    public string easyWord1 = "Bone";
+    public string easyWord2 = "Heart";
+    public string easyWord3 = "Brain";
+    public string easyWord4 = "Skin";
+    public string easyWord5 = "Flesh";
+
+    public string mediumWord1 = "Muscle";
+    public string mediumWord2 = "Esophagus";
+    public string mediumword3 = "Clavicle";
+    public string mediumword4 = "Abscess";
+    public string mediumWord5 = "Hypertension";
     void Start()
     {
         
